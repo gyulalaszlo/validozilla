d7796f8f750da0b3fd8e25f1f8f374c6bfbcb506
Gyula László <gyula.laszlo.gm@gmail.com>
2010-01-23 17:28:04 +0100

    new test for generators

---
+++ test/test_test_ruby_generator.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+class TestTestRubyGenerator < Test::Unit::TestCase
+  def setup
+  end
+  
+  def teardown
+  end
+  
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
